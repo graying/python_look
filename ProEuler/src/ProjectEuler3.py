17b67adb973ebef6051f3d307953e822d8da8576
graying <graying@gmail.com>
2017-01-13 01:01:46 +0800

    reformatted

--- ProEuler/src/ProjectEuler3.py
+++ ProEuler/src/ProjectEuler3.py
@@ -9,31 +9,33 @@ What is the largest prime factor of the number 600851475143 ?
 '''
 import math
 
+
 def isPrime(num):
     '''
     return True if the given number is Prime, otherwise return False
 
     '''
-    if num==2:
+    if num == 2:
         return True
-    if num % 2 ==0:
+    if num % 2 == 0:
         return False
-      #print(halfnum)
+        # print(halfnum)
     for i in range(3, int(math.ceil(math.sqrt(num)) + 1), 2):
-        #Learned this mothed from http://codereview.stackexchange.com/questions/11317/prime-factorization-of-a-number
+        # Learned this mothed from http://codereview.stackexchange.com/questions/11317/prime-factorization-of-a-number
         if num % i == 0:
-            #print (str(num)+" can be divided by "+str(i)+"="+str(num/i))
+            # print (str(num)+" can be divided by "+str(i)+"="+str(num/i))
             return False
     return True
 
-if __name__=="__main__":
-    S=600851475143
-    N=int(math.ceil(math.sqrt(S)) + 1)
 
-    while N>2:
-        print ("trying "+str(N))
-        if (S % N ==0):
+if __name__ == "__main__":
+    S = 600851475143
+    N = int(math.ceil(math.sqrt(S)) + 1)
+
+    while N > 2:
+        print ("trying " + str(N))
+        if (S % N == 0):
             if isPrime(N):
                 print (N)
                 break
-        N = N-1
+        N = N - 1
