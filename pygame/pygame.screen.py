456be9e3934320fb5393f15ee0ec4d8a9103d88b
graying <graying@gmail.com>
2017-01-16 00:43:24 +0800

    updated

--- pygame/pygame.screen.py
+++ pygame/pygame.screen.py
@@ -1,26 +1,28 @@
-import pygame
-import time
 import random
+import time
+
+import pygame
 
 pygame.init()
 screen = pygame.display.set_mode([1440, 900])
-screen.fill([2,255,2])
+screen.fill([0, 0, 0])
 
-rectcolor = [random.randint(0,255), random.randint(0,255), random.randint(0, 255)]
-rectsize = [random.randint(0,1000), random.randint(0,900), random.randint(0,256), random.randint(0, 256)]
-pygame.draw.rect(screen, rectcolor, rectsize, random.randint(0,5))
+rectcolor = [random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)]
+rectsize = [random.randint(0, 1000), random.randint(0, 900), random.randint(0, 256), random.randint(0, 256)]
+pygame.draw.rect(screen, rectcolor, rectsize, random.randint(0, 5))
 pygame.display.flip()
 
 for i in range(100):
-    rectcolor = [random.randint(0,255), random.randint(0,255), random.randint(0, 255)]
-    rectsize = [random.randint(0,1000), random.randint(0,900), random.randint(0,256), random.randint(0, 256)]
-    pygame.draw.rect(screen, rectcolor, rectsize, random.randint(0,5))
+    rectcolor = [random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)]
+    rectsize = [random.randint(0, 1220), random.randint(0, 800), random.randint(0, 256), random.randint(0, 256)]
+    pygame.draw.rect(screen, rectcolor, rectsize, random.randint(0, 5))
     pygame.display.flip()
+    time.sleep(1)
 
 running = True
 while running:
     for event in pygame.event.get():
-        if event.type==pygame.QUIT:
+        if event.type == pygame.QUIT:
             running = False
 
 pygame.quit()
