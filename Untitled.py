9cff63ef0bc74ec45a534c146dcb8477f8b09441
graying <graying@gmail.com>
2018-12-30 19:46:20 +0800

    updated gitignore...

--- Untitled.py
+++ Untitled.py
@@ -1,9 +1,21 @@
+import os
 import array
 import binascii
 
-s = 'this is the array.'
-a = array.array('c', s)
+#s = 'this is the array.'
+#a = array.array('c', s)
 
-print 'As string:', s
-print 'As array :', a
-print a[1], a[2], a[3]
+#print 'As string:', s
+#print 'As array :', a
+#print a[1], a[2], a[3]
+
+thread = os.fork()
+if thread == 0:
+    print ("this is in child processing...")
+#    while True:
+#        print ("pid = 0")
+
+else:
+    print ("this is in parent processing...")
+    #while True:
+#        print ("pid = " + str(pid))
